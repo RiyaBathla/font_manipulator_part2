143def82365f37c5c7d98261c6424296ea469765
RiyaBathla <92024130+RiyaBathla@users.noreply.github.com>
2024-03-04 12:52:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+noseX=0;
+noseY=0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results)
+{
+    if(results.length >0)
+    {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +"noseY = "+ noseY)
+    }
+
+}
